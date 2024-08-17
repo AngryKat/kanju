bb8acaf6514593add98b2decf2e5b4994f20ee09
AngryKat <gatto_candido@ukr.net>
2024-08-17 22:28:34 +0300

    Remove async

--- utils/kanji-async-storage.ts
+++ utils/kanji-async-storage.ts
@@ -12,7 +12,7 @@ export async function initKanjis() {
   }
 }
 
-export async function getDictionaryEntries() {
+export function getDictionaryEntries() {
   if (!kanjis) throw new Error("Kanjis are not initialized");
   const arrayUniqueByKey = [
     ...new Map(
@@ -24,7 +24,7 @@ export async function getDictionaryEntries() {
   return arrayUniqueByKey.filter((entry) => !!entry.meaning);
 }
 
-export async function getKanjis() {
+export function getKanjis() {
   if (!kanjis) throw new Error("Kanjis are not initialized");
   return kanjis;
 }
@@ -39,7 +39,7 @@ export async function removeKanjiById(kanjiId: string) {
   await AsyncStorage.setItem("kanjis", JSON.stringify(kanjis));
 }
 
-export async function getKanjiById(kanjiId: string) {
+export function getKanjiById(kanjiId: string) {
   if (!kanjis) throw new Error("Kanjis are not initialized");
   return kanjis[kanjiId];
 }
@@ -49,7 +49,8 @@ export async function editKanji(
   newData: Partial<Omit<Kanji, "id" | "kanji">>
 ) {
   if (!kanjis) throw new Error("Kanjis are not initialized");
-  const kanjiEdited = await getKanjiById(kanjiId);
-  const newKanji = { ...kanjiEdited, ...newData };
+  const kanjiToEdit = getKanjiById(kanjiId);
+  const newKanji = { ...kanjiToEdit, ...newData };
   kanjis[kanjiId] = { ...newKanji };
+  await AsyncStorage.setItem("kanjis", JSON.stringify(kanjis));
 }
