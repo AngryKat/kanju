13867bfdf1f50a5ad981e9ba01f7bf57b3fb364f
AngryKat <gatto_candido@ukr.net>
2024-09-08 19:44:02 +0300

    Add redux persist

--- hooks/use-search-bar.ts
+++ hooks/use-search-bar.ts
@@ -12,12 +12,12 @@ const getValueAtPath = <T>(obj: T, path: string) => {
 export const useSearchBar = <T>(
   data: T[],
   byKeys: string[],
-  placeholder = "Пошук...",
+  placeholder = "Пошук..."
 ) => {
   const navigation = useNavigation();
   const [searchText, onChangeSearch] = useState("");
   const [filteredData, setFilteredData] = useState<T[]>(data);
-  const filterKeys = useRef(byKeys)
+  const filterKeys = useRef(byKeys);
 
   useEffect(() => {
     if (searchText === "") {
@@ -53,4 +53,45 @@ export const useSearchBar = <T>(
   }, [navigation]);
 
   return filteredData;
+};
+export const useSearchBar2 = <T>(
+  data: T[],
+  byKeys?: Array<keyof T extends string ? keyof T : never>
+) => {
+  const [searchText, setSearchText] = useState("");
+  const navigation = useNavigation();
+  const filterKeys = useRef(byKeys);
+
+  const filtered = useMemo(
+    () =>
+      data?.filter((item) => {
+        if (typeof item === "string") {
+          return item === searchText;
+        }
+        return filterKeys.current?.reduce((acc: boolean, key: string) => {
+          return (
+            acc ||
+            (getValueAtPath(item, key) as string)
+              .toLowerCase()
+              .includes(searchText.toLowerCase())
+          );
+        }, false);
+      }),
+    [searchText, filterKeys, data]
+  );
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerSearchBarOptions: {
+        placeholder: "Search...",
+        hideWhenScrolling: false,
+        onChangeText: (event: { nativeEvent: { text: string } }) => {
+          // onSearch(event.nativeEvent.text);
+          setSearchText(event.nativeEvent.text);
+        },
+      },
+    });
+  }, [navigation]);
+
+  return filtered;
 };
